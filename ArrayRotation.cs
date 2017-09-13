541cae45d8de6fda8810d5e64644dfbc39d0a437
rushaniia.badrutdinova <rushaniia.badrutdinova@gmail.com>
2017-09-13 10:18:26 +0300

    Add files via upload

---
+++ ArrayRotation.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+        string[] tokens_n = Console.ReadLine().Split(' ');
+        int n = Convert.ToInt32(tokens_n[0]);
+        int k = Convert.ToInt32(tokens_n[1]);
+        string[] a_temp = Console.ReadLine().Split(' ');
+        int[] a = Array.ConvertAll(a_temp,Int32.Parse);
+        int[] newArray = new int[n]; 
+        for(int i = 0; i < n; i++){
+          int newLocation = (i + k) % n;
+          newArray[i] = a[newLocation];
+         }
+        string result= String.Join(" ", newArray);
+        Console.WriteLine(result);
+        
+    }
+}
